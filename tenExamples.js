e10268f30b36fe5e179731561e0f71f26579e90f
richardmusic <146582773+richardmusic@users.noreply.github.com>
2023-12-27 15:01:03 -0700

    Add files via upload

---
+++ tenExamples.js
@@ -0,0 +1,154 @@
+const multiExample1 = [
+    "湯が<span style='color: #2e2e2e;'>沸いた</span> (Yu ga waita) The water boiled<br>",
+    "<span style='color: #2e2e2e;'>沸騰する</span>前に止めて (Futtou suru mae ni tomete) Stop before it boils<br>",
+    "やかんで水を<span style='color: #2e2e2e;'>沸かす</span> (Yakan de mizu o wakasu) Boiling water in a kettle<br>",
+    "<span style='color: #2e2e2e;'>沸騰している</span>湯気がすごい (Futtou shiteiru yuge ga sugoi) The steam from boiling is intense<br>",
+    "<span style='color: #2e2e2e;'>沸騰する</span>と音がする (Futtou suru to oto ga suru) There's a sound when it boils<br>",
+    "<span style='color: #2e2e2e;'>沸騰したら</span>すぐ火を止めて (Futtou shitara sugu hi o tomete) Turn off the heat as soon as it boils<br>",
+    "<span style='color: #2e2e2e;'>沸騰する</span>まで待って (Futtou suru made matte) Wait until it boils<br>",
+    "<span style='color: #2e2e2e;'>沸騰した</span>熱いお湯がほしい (Futtou shita atsui oyu ga hoshii) I want boiling hot water<br>",
+    "<span style='color: #2e2e2e;'>沸騰する</span>前に取り出して (Futtou suru mae ni toridashite) Take it out before it boils<br>",
+    "湯を<span style='color: #2e2e2e;'>沸かす</span>ときに火を強くする (Yu o wakasu toki ni hi o tsuyoku suru) Increase the heat when boiling water<br>",
+    "鍋で湯を<span style='color: #2e2e2e;'>沸かしています</span> (Nabe de yu o wakashiteimasu) Boiling water in the pot<br>",];
+const multiExample2 = [
+    "<span style='color: #2e2e2e;'>需要</span>が高いです Juyou ga takai desu The demand is high<br>",
+    "その商品は<span style='color: #2e2e2e;'>需要</span>があります Sono shouhin wa juyou ga arimasu - There is demand for that product<br>",
+    "<span style='color: #2e2e2e;'>需要</span>を満たす必要があります Juyou o mitasu hitsuyou ga arimasu - There is a need to meet the demand<br>",
+    "<span style='color: #2e2e2e;'>需要</span>が急速に増えています Juyou ga kyuusoku ni fueteimasu - The demand is increasing rapidly<br>",
+    "<span style='color: #2e2e2e;'>需要予測</span>が難しいです Juyou yosoku ga muzukashii desu - Forecasting demand is difficult<br>",
+    "そのサービスには<span style='color: #2e2e2e;'>需要</span>がありますか？ Sono saabisu ni wa juyou ga arimasu ka? - Is there a demand for that service?<br>",
+    "<span style='color: #2e2e2e;'>需要</span>に応じて製品を調整します Juyou ni oujite seihin o chousei shimasu - We adjust the products according to the demand<br>",
+    "<span style='color: #2e2e2e;'>需要</span>を満たすために努力しています Juyou o mitasu tame ni doryoku shiteimasu. - We are making efforts to meet the demand<br>",
+    "<span style='color: #2e2e2e;'>需要</span>が予想以上でした Juyou ga yosou ijou deshita. - The demand exceeded expectations<br>",
+    "<span style='color: #2e2e2e;'>需要</span>が安定しています Juyou ga antei shiteimasu. - The demand is stable<br>",];
+const multiExample3 = [
+    "虫刺されで<span style='color: #2e2e2e;'>腫れた</span> (Mushi sasare de hareta) I got swollen from a bug bite<br>",
+    "目が<span style='color: #2e2e2e;'>腫れています</span> (Me ga harete imasu) My eyes are swollen<br>",
+    "喉が<span style='color: #2e2e2e;'>腫れている</span>ようだ (Nodo ga harete iru you da) Looks like my throat is swollen<br>",
+    "けがで足が<span style='color: #2e2e2e;'>腫れました</span> (Kega de ashi ga hare mashita) My leg got swollen from the injury<br>",
+    "その蚊に刺されて<span style='color: #2e2e2e;'>腫れた</span> (Sono ka ni sasarete hareta) I got swollen from that mosquito bite<br>",
+    "手が<span style='color: #2e2e2e;'>腫れてて</span>動かせない (Te ga harete te ugokasenai) My hand is swollen, and I can't move it<br>",
+    "彼の体に<span style='color: #2e2e2e;'>腫瘍</span>が見つかった (Kare no karada ni shuyou ga mitsukatta) A tumor was found in his body<br>",
+    "<span style='color: #2e2e2e;'>腫れ</span>が引かない (Hare ga hikanai) The swelling isn't going down<br>",
+    "<span style='color: #2e2e2e;'>腫瘍</span>が原因で痛みが出ています (Shuyou ga gen'in de itami ga dete imasu) Pain is occurring due to the tumor<br>",
+    "<span style='color: #2e2e2e;'>腫れ止め</span>の薬を塗った (Hare dome no kusuri o nutta) I applied some anti-swelling medicine<br>",];
+const multiExample4 = [
+    "彼は<span style='color: #2e2e2e;'>疾患</span>を抱えています (Kare wa shikkan o kakaete imasu) He has a medical condition<br>",
+    "<span style='color: #2e2e2e;'>疾患</span>の症状が出てきた (Shikkan no shoujou ga dete kita) The symptoms of the illness appeared<br>",
+    "<span style='color: #2e2e2e;'>疾病</span>を治療する必要があります (Shippei o chiryou suru hitsuyou ga arimasu) There is a need to treat the disease<br>",
+    "<span style='color: #2e2e2e;'>疾患</span>の治療法を探しています (Shikkan no chiryouhou o sagashite imasu) Looking for treatment options for the illness<br>",
+    "その<span style='color: #2e2e2e;'>疾患</span>は遺伝的な要素がある (Sono shikkan wa iden-teki na youso ga aru) That illness has a genetic component<br>",
+    "<span style='color: #2e2e2e;'>疾患</span>の早期発見が重要です (Shikkan no souki hakken ga juuyou desu) Early detection of the illness is important<br>",
+    "<span style='color: #2e2e2e;'>疾患</span>のリスクを減らす方法を知りたい (Shikkan no risuku o herasu houhou o shiritai) I want to know ways to reduce the risk of illness<br>",
+    "彼は病気で<span style='color: #2e2e2e;'>疾走</span>している (Kare wa byouki de shissou shite iru) He's running a fever due to sickness<br>",
+    "その<span style='color: #2e2e2e;'>疾患</span>はかなり重篤です (Sono shikkan wa kanari jutoku desu) That illness is quite severe<br>",
+    "<span style='color: #2e2e2e;'>疾病</span>にかかるのを予防するために手を洗いましょう (Shippei ni kakaru no o yobou suru tame ni te o araimashou) Translation: Let's wash our hands to prevent illness"
+];
+const multiExample5 = [
+    "<span style='color: #2e2e2e;'>匿名</span> で投稿することができます (Tokumei de toukou suru koto ga dekimasu) Translation: You can post anonymously<br>",
+    "<span style='color: #2e2e2e;'>匿名性</span> を保つために、偽名を使うことがあります (Tokumei-sei o tamotsu tame ni, gimei o tsukau koto ga arimasu) Translation: To maintain anonymity, one may use a false name<br>",
+    "<span style='color: #2e2e2e;'>匿名性</span>を重んじる人もいます (Tokumeisei o omonjiru hito mo imasu.) There are people who value anonymity)<br>",
+    "彼は<span style='color: #2e2e2e;'>匿名</span>でそのフォーラムに投稿した (Kare wa tokumei de sono fo-ramu ni toukou shita) He posted on that forum anonymously<br>",
+    "私は<span style='color: #2e2e2e;'>匿名</span>の手紙を受け取ったことがあります (Watashi wa tokumei no tegami o uketotta koto ga arimasu) I have received an anonymous letter before<br>",
+    "<span style='color: #2e2e2e;'>匿名性</span>を保つことはオンラインのセキュリティに関して重要です (Tokumeisei o tamotsu koto wa onrain no sekuriti ni kan shite juuyou desu) Maintaining anonymity is important for online security<br>",
+    "そのウェブサイトでは、<span style='color: #2e2e2e;'>匿名</span>で質問をすることができます (Sono webusaito de wa, tokumei de shitsumon o suru koto ga dekimasu) On that website, you can ask questions anonymously<br>",
+    "<span style='color: #2e2e2e;'>匿名性</span>を守るために、プライバシー設定を確認しましょう (Tokumeisei o mamoru tame ni, puraibashii settei o kakunin shimashou) To protect anonymity, let's check the privacy settings<br>",
+    "彼は<span style='color: #2e2e2e;'>匿名</span>で寄付をした (Kare wa tokumei de kifu o shita) He made a donation anonymously<br>",
+    "<span style='color: #2e2e2e;'>匿名</span>の通報者が事件の解決に役立った (Tokumei no tsuuhousha ga jiken no kaiketsu ni yakudatta) An anonymous tipster helped solve the case<br>",
+];
+const multiExample6 = [
+    "5asdf",
+    "5",
+    "5",
+];
+const multiExample7 = [
+    "5",
+    "5",
+    "5",
+];
+const multiExample8 = [
+    "5",
+    "5",
+    "5",
+];
+const multiExample9 = [
+    "5",
+    "5",
+    "5",
+];
+const multiExample10 = [
+    "5",
+    "5",
+    "5",
+];
+const multiExample12 = [
+    "5",
+    "5",
+    "5",
+];
+const multiExample13 = [
+    "5",
+    "5",
+    "5",
+];
+const multiExample14 = [
+    "5",
+    "5",
+    "5",
+];
+const multiExample15 = [
+    "5",
+    "5",
+    "5",
+];
+const multiExample16 = [
+    "5",
+    "5",
+    "5",
+];
+const multiExample17 = [
+    "5",
+    "5",
+    "5",
+];
+const multiExample18 = [
+    "5",
+    "5",
+    "5",
+];
+const multiExample19 = [
+    "5",
+    "5",
+    "5",
+];
+const multiExample20 = [
+    "5",
+    "5",
+    "5",
+];
+const multiExample11 = [
+    "5",
+    "5",
+    "5",
+];
+
+export {multiExample1};
+export {multiExample2};
+export {multiExample3};
+export {multiExample4};
+export {multiExample5};
+export {multiExample6};
+export {multiExample7};
+export {multiExample8};
+export {multiExample9};
+export {multiExample10};
+export {multiExample11};
+export {multiExample12};
+export {multiExample13};
+export {multiExample14};
+export {multiExample15};
+export {multiExample16};
+export {multiExample17};
+export {multiExample18};
+export {multiExample19};
+export {multiExample20};
